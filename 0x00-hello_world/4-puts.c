3ebb90787999029089212dcf6f303888797a4323
Khadime <Khadimediongue45@gmail.com>
2022-09-10 08:20:19 -0700

    updates

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Main - Entry point
+ *
+ * Result: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle");
+	return(0);
+}
