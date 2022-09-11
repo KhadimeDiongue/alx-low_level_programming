5d401eefbada17ef51cbe31a9f913d86ec7302a8
Khadime <Khadimediongue45@gmail.com>
2022-09-11 14:15:44 -0700

    updates

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,13 @@ int main(void)
 	while (number < 10)
 	{
 		putchar(number + '0');
-		if (number < 9){
+		if (number < 9)
+		{
 			putchar(',');
 			putchar(' ');
 		}
 	number++;
 	}
+	putchar('\n');
 	return (0);
 }
