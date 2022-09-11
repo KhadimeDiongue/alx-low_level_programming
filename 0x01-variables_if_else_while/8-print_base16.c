12a7a671f7da0a4465039fac94f4d4dfea1cfcf2
Khadime <Khadimediongue45@gmail.com>
2022-09-11 13:10:49 -0700

    updates

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,15 @@
 int main(void)
 {
 	int numbers = 0;
+	char letters = 'a';
 
 	while (numbers < 16)
-	{
-		putchar(numbers + '0');
+	{	
+		if (numbers < 9)
+			putchar(numbers + '0');
+		else
+			putchar(letters);
+		letters++;
 		numbers++;
 	}
 	putchar('\n');
