dd5527a0187d7cc23ced7f5d47902a018840fc11
Khadime <Khadimediongue45@gmail.com>
2022-09-11 11:38:32 -0700

    updadatess

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alphabet = a;
+
+	while (alphabet < 'z')
+	{
+		putchar(alphabet);
+		alphabeth++;
+	{
+	return (0);
+
+}
