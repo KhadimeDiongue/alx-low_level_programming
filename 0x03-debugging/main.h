8a6e0f61833a835dc2cad5aa2244337d187e4f90
Khadime <Khadimediongue45@gmail.com>
2022-09-16 14:59:08 -0700

    updates

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN.H
+#define MAIN.H
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -13,3 +15,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+#endif
