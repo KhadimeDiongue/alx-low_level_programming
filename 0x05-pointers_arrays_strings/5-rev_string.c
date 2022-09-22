f557ba342a9ff2df5bfe8c9404bd64de291651ec
Khadime <Khadimediongue45@gmail.com>
2022-09-21 19:42:42 -0700

    updates

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -23,12 +23,11 @@ void rev_string(char *s)
 		len++;
 	}
 	l = len - 1;
-	for( ; e < ((l / 2) + 1); e++)
+	for ( ; e < ((l / 2) + 1); e++)
 	{
 		x = (l - e);
 		n = s[e];
 		s[e] = s[x];
 		s[x] = n;
 	}
-}
-		
+}		
