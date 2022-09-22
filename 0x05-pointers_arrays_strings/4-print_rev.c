3186d4868444204d3206aab05bfd8871e00ce321
Khadime <Khadimediongue45@gmail.com>
2022-09-21 19:13:06 -0700

    updates

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,7 +12,7 @@ void print_rev(char *s)
 {
 	int length;
 
-	for (length = 0; length != '\0'; length++)
+	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
 	for (length = length - 1; length  >= 0; length--)
