dfd55908054918400fbe7d4b1240e3ae52e392a3
Khadime <Khadimediongue45@gmail.com>
2022-09-27 15:21:18 -0700

    updates

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,9 +4,11 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string
- * @accept: contains bytes that may or may not compose parts of the string
+ * @accept: contains bytes that may or may not compose 
+ * parts of the string
  *
- * Return: the number of bytes that compose the length
+ * Return: the number of bytes that compose the 
+ * length
  */
 unsigned int _strspn(char *s, char *accept)
 {
